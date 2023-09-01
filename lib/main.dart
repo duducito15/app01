af1e362b35bce9077b64a396e1c6ecf6b46cf301
Eduardo Chavez <echavezv@utea.edu.pe>
2023-09-01 07:36:49 -0500

    Widget Column

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,29 @@ void main(List<String> args) {
       home: Scaffold(
         backgroundColor: Color(0xffccd5ae),
         body: SafeArea(
-          child: Container(
-            child: Text("Hola!!! a todos"),
-            color: Colors.yellow,
-            height: 100.0,
-            width: 200.0,
-            margin: //EdgeInsets.all(30.0),
-                //EdgeInsets.symmetric(horizontal: 50.0, vertical: 100.0),
-                EdgeInsets.only(top: 200.0, left: 60.0),
-            padding: EdgeInsets.all(20.0),
-            alignment: Alignment.bottomRight,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Text("Algoritmos 1"),
+                color: Colors.amberAccent,
+                height: 50.0,
+                //alignment: Alignment.center,
+              ),
+              Container(
+                child: Text("Fundamentos de programación"),
+                color: Colors.blueAccent,
+                height: 50.0,
+                // alignment: Alignment.center,
+              ),
+              Container(
+                child: Text("Ingenieria de software 2"),
+                color: Colors.redAccent,
+                height: 50.0,
+                //alignment: Alignment.center,
+              ),
+            ],
           ),
         ),
       ),
