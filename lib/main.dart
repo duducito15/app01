a467df5c278656fa492f26f1deb288e467e07bf8
Eduardo Chavez <echavezv@utea.edu.pe>
2023-09-01 07:03:28 -0500

    Widget Container

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor:
-            //Colors.blueGrey,
-            Color(0xffccd5ae),
-        body: Center(
-          child: Text("Holaaaaaaa"),
+        backgroundColor: Color(0xffccd5ae),
+        body: SafeArea(
+          child: Container(
+            child: Text("Hola!!! a todos"),
+            color: Colors.yellow,
+            height: 100.0,
+            width: 200.0,
+            margin: //EdgeInsets.all(30.0),
+                //EdgeInsets.symmetric(horizontal: 50.0, vertical: 100.0),
+                EdgeInsets.only(top: 200.0, left: 60.0),
+            padding: EdgeInsets.all(20.0),
+            alignment: Alignment.bottomRight,
+          ),
         ),
       ),
     ),
