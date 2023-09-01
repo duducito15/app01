be5d2b5a658fed20653e8ea6fab781d448b820f1
Eduardo Chavez <echavezv@utea.edu.pe>
2023-09-01 06:39:08 -0500

    Widget Center

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,13 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         backgroundColor:
             //Colors.blueGrey,
             Color(0xffccd5ae),
-        body: SafeArea(
-          child: Text(
-            "Hola a todos!!!",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30.0,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
+        body: Center(
+          child: Text("Holaaaaaaa"),
         ),
       ),
     ),
